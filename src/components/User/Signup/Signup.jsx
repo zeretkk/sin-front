282c6d4ba300dda2b1a69da866aa7a95e1cb12bf
zeret <kirill03192@gmail.com>
2023-02-16 12:17:43 +0300

    Add registration

--- src/components/User/Signup/Signup.jsx
+++ src/components/User/Signup/Signup.jsx
@@ -1,24 +1,54 @@
 import c from "../Login/login.module.scss";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import {useState} from "react";
-import {useDispatch} from "react-redux";
-import {register} from "../../../slices/userSlice";
+import axios from "axios";
 
 function Signup() {
     const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+    const [name, setName] = useState('')
     const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     const [submitPassword, setSubmitPassword] = useState('')
-    const dispatch = useDispatch()
+    const [validity, setValidity] = useState({})
+    const [error, setError] = useState(false)
+    const navigate = useNavigate()
 
     const submitForm = (e) => {
-      e.preventDefault()
-        dispatch(register({
-            username,
-            email,
-            pass:password,
-        }))
-
+        e.preventDefault()
+        if(validateForm()){
+            setError(false)
+            axios.post('http://localhost:3001/user', {pass: password, username, name, email})
+                .then(r =>{
+                    navigate('/login')
+                })
+                .catch(err=>{
+                    console.log(err.response.data)
+                    switch (err.response.data.type){
+                        case 'unique':
+                            setValidity({...validity, username: 'Username is already taken'})
+                            break
+                        default:
+                            setError(true)
+                    }
+                })
+        }
+    }
+    const validateForm=()=>{
+        let errObj = {}
+        if(username.length < 3){
+            errObj = {...errObj, username: 'Must be at least 3 characters long'}
+        }
+        if(name.length < 3){
+            errObj = {...errObj, name: 'Must be at least 3 characters long'}
+        }
+        if(password.length < 6){
+            errObj = {...errObj, password: 'Must be at least 6 characters long'}
+        }
+        if(submitPassword !== password){
+            errObj = {...errObj, submitPassword: 'Passwords must match'}
+        }
+        setValidity(errObj)
+        return !Object.keys(errObj).length>0
     }
     return (
         <div className={c.wrapper}>
@@ -26,28 +56,35 @@ function Signup() {
             <form action="/" noValidate className={c.form} onSubmit={submitForm}>
                 <div className={c.group}>
                     <h2 className={c.heading}>SignUp</h2>
+                    {error ?? <p className={c.error}>Something went wrong</p>}
                 </div>
                 <div className={c.group}>
-                    <label htmlFor='username' className={c.label}>Username</label>
-                    <input type="text" id='username' name='username' placeholder='username' className={c.input}
+                    <label htmlFor='name' className={c.label}>Name <span className={c.errorLabel}>{validity?.name}</span></label>
+                    <input type="text" id='name' name='name' placeholder='name' className={`${c.input} ${validity?.name?c.errorInput:''}`}
+                           value={name} onChange={(e)=>setName(e.target.value)}
+                    />
+                </div>
+                <div className={c.group}>
+                    <label htmlFor='username' className={c.label}>Username <span className={c.errorLabel}>{validity?.username}</span></label>
+                    <input type="text" id='username' name='username' placeholder='username' className={`${c.input} ${validity?.username?c.errorInput:''}`}
                            value={username} onChange={(e)=>setUsername(e.target.value)}
                     />
                 </div>
                 <div className={c.group}>
-                    <label htmlFor='password' className={c.label}>E-mail</label>
-                    <input type="email" id='email' name='email' placeholder='email' className={c.input}
+                    <label htmlFor='password' className={c.label}>E-mail <span className={c.errorLabel}>{validity?.email}</span></label>
+                    <input type="email" id='email' name='email' placeholder='email' className={`${c.input} ${validity?.email?c.errorInput:''}`}
                            value={email} onChange={(e)=>setEmail(e.target.value)}
                     />
                 </div>
                 <div className={c.group}>
-                    <label htmlFor='password' className={c.label}>Password</label>
-                    <input type="password" id='password' name='password' placeholder='password' className={c.input}
+                    <label htmlFor='password' className={c.label}>Password <span className={c.errorLabel}>{validity?.password}</span></label>
+                    <input type="password" id='password' name='password' placeholder='password' className={`${c.input} ${validity?.password?c.errorInput:''}`}
                            value={password} onChange={(e)=>setPassword(e.target.value)}
                     />
                 </div>
                 <div className={c.group}>
-                    <label htmlFor='password' className={c.label}>Submit Password</label>
-                    <input type="password" id='repassword' name='repassword' placeholder='submit password' className={c.input}
+                    <label htmlFor='repassword' className={c.label}>Submit Password <span className={c.errorLabel}>{validity?.submitPassword}</span></label>
+                    <input type="password" id='repassword' name='repassword' placeholder='submit password' className={`${c.input} ${validity?.submitPassword?c.errorInput:''}`}
                            value={submitPassword} onChange={(e)=>setSubmitPassword(e.target.value)}
                     />
                 </div>
--- src/slices/userSlice.js
+++ src/slices/userSlice.js
@@ -1,31 +1,16 @@
 import {createSlice} from "@reduxjs/toolkit";
 
+
 const initialState = {
     value: {
         data:null,
-        loading:false,
         error:false
     }
 }
-
 export const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers:{
-        register: (state, action)=>{
-               fetch('http://localhost:3001/user', {
-                    method: 'POST',
-                    headers:{
-                        'Content-Type':'application-json; charset=utf-8'
-                    },
-                    body: JSON.stringify(action.payload)
-                })
-                   .then(response=>response.json())
-                   .then(data=>state.value= {...state.value, user:data})
-                   .catch(err=>{
-                       console.log(err)
-                   })
-        }
     }
 })
 export const {register} = userSlice.actions
