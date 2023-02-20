393ac56f86f25fdf6eda4ce813ecd11c69bb53ea
zeret <kirill03192@gmail.com>
2023-02-20 14:47:34 +0300

    Content store

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,35 +1,14 @@
 import c from './categories.module.scss'
-import Art from '../../assets/images/Categories/Art.png'
-import Collectibles from '../../assets/images/Categories/Collectibles.png'
-import Music from '../../assets/images/Categories/Music.png'
-import Photography from '../../assets/images/Categories/Photography.png'
-import Sport from '../../assets/images/Categories/Sport.png'
-import Utility from '../../assets/images/Categories/Utility.png'
-import Video from '../../assets/images/Categories/Video.png'
-import VirtualWorlds from '../../assets/images/Categories/VirtualWorlds.png'
-
-import Basketball from '../../assets/icons/category/Basketball.svg'
-import MusicNotes from '../../assets/icons/category/MusicNotes.svg'
-import Camera from '../../assets/icons/category/Camera.svg'
-import MagicWand from '../../assets/icons/category/MagicWand.svg'
-import PaintBrush from '../../assets/icons/category/PaintBrush.svg'
-import Swatches from '../../assets/icons/category/Swatches.svg'
-import VideoCamera from '../../assets/icons/category/VideoCamera.svg'
-import Planet from '../../assets/icons/category/Planet.svg'
 import CategoryCard from "./CategoryCard";
+import {useSelector} from "react-redux";
+
 export default function Categories() {
+    const categories = useSelector(state => state.content.value.categories)
     return (
         <section className={c.wrapper}>
             <h3 className={c.heading}>Browse Categories</h3>
             <div className={c.list}>
-                <CategoryCard title={'Art'} background={Art} icon={PaintBrush}/>
-                <CategoryCard title={'Collectibles'} background={Collectibles} icon={Swatches}/>
-                <CategoryCard title={'Music'} background={Music} icon={MusicNotes}/>
-                <CategoryCard title={'Photography'} background={Photography} icon={Camera}/>
-                <CategoryCard title={'Sport'} background={Sport} icon={Basketball}/>
-                <CategoryCard title={'Utility'} background={Utility} icon={MagicWand}/>
-                <CategoryCard title={'Video'} background={Video} icon={VideoCamera}/>
-                <CategoryCard title={'VirtualWorlds'} background={VirtualWorlds} icon={Planet}/>
+                {categories? categories.map((category,i)=><CategoryCard key={'mainCategory-'+i} category={category}/>):null}
 
             </div>
 
