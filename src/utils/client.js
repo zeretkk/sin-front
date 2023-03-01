d75a18b479d845848c10b5b9de08ce87805e82a4
zeret <kirill03192@gmail.com>
2023-03-01 11:24:35 +0300

    Add axios unauth handler

--- src/utils/client.js
+++ src/utils/client.js
@@ -9,5 +9,17 @@ api.interceptors.request.use(cfg=>{
         cfg.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
     }
     return cfg
+}, async (err)=>{
+    if(err.response.status === 401 && err.config && !err.config._isRetry){
+        err.config._isRetry = true
+        try {
+            const response = await api.get('/user/refresh')
+            localStorage.setItem('token', response.data.accessToken)
+            return api.request(err.config)
+        }catch (e) {
+            localStorage.removeItem('token')
+        }
+    }
+    throw err
 })
 export default api
